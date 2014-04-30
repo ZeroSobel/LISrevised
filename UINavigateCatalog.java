1912f16b342abd7d4102feebd74a185df5f826ea
Tyler Eason <annoyingjk3@yahoo.com>
2014-04-29 20:28:58 -0500

    Added UINavigate Catalog

---
+++ UINavigateCatalog.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+import java.lang.String;
+
+public class UINavigateCatalog extends UserInterface {
+   public static final String prompt = "Enter a search query, or type \"goback\" to return to the main menu: ";
+   public static final String sagain = "\nWould you like to search again? y/n";
+   
+   
+   public UINavigateCatalog() {
+   
+   }
+   
+   
+   public static void show(boolean loggedIn, boolean admin) {
+      boolean repeat = true;
+      String selection = "";
+      Scanncer sc = new Scanner(System.in);
+      System.out.prinln(prompt);
+      selection = sc.nextLine();
+      execute(selection, loggedIn, admin);
+   }
+   
+   public static void execute(String input, boolean loggedIn, boolean admin) {
+      Scanner sc = new Scanner(System.in);
+      String selection = "";
+      if(input.compareTo("goback") == 0) {
+         UIMainScreen.show(loggedIn, admin);
+      }
+      else {
+         System.out.println("\n" + CatalogController.search(selection));
+         System.out.println(sagain);
+         selection = sc.nextLine();
+         if(selection.compareToIgnoreCase("y") == 0) {
+            UINavigateCatalog.show(loggedIn, admin);
+         }
+         else {
+            UIMainScreen.show(loggedIn, admin);
+         }
+      }
+   }
+}
